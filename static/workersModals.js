99a8149e519a62203264bd8aa878f18736f8e296
Jimmy LABs <tmayojimmy@gmail.com>
2025-01-31 21:01:05 -0500

    Se añaden los botones de editar trabajador y eliminar trabajador.

--- static/workersModals.js
+++ static/workersModals.js
@@ -15,8 +15,19 @@ editModal.addEventListener('show.bs.modal', event => {
     const email = button.getAttribute('data-bs-email');
     // Update the modal's content
     editModalIdInput.value = id;
-    editModalNameInput.value = 'Arroz';
+    editModalNameInput.value = name;
     editModalBranchSelect.value = branch;
     editModalPositionSelect.value = position;
     editModalEmailInput.value = email;
+});
+
+const deleteModal = document.getElementById('deleteModal');
+const deleteModalIdInput = deleteModal.querySelector('#delete-id');
+const deleteModalNameInput = deleteModal.querySelector('#delete-name');
+deleteModal.addEventListener('show.bs.modal', event => {
+    const button = event.relatedTarget;
+    const id = button.getAttribute('data-bs-id');
+    const name = button.getAttribute('data-bs-name');
+    deleteModalNameInput.textContent = name;
+    deleteModalIdInput.value = id;
 });
